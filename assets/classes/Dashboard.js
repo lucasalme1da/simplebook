b3b008ce6c7b562cbbfd39927bca0e979542deee
Lucas Almeida <lucasdealmeida.ss@gmail.com>
2019-05-11 12:08:50 -0300

    Excluido arquivos desnecessarios, desenvolvidas algumas classes, testes realizados no estilo

---
+++ assets/classes/Dashboard.js
@@ -0,0 +1,12 @@
+class Dashboard {
+  constructor(bodyRef) {
+    this.bodyRef = bodyRef;
+  }
+
+  criar() {
+    this.ref = document.createElement("dashboard");
+    this.bodyRef.appendChild(this.ref);
+  }
+}
+
+module.exports = Dashboard;
