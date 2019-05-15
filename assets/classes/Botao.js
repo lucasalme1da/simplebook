fff0d1465ba0c202c4c87b3996a0602a78dba7ba
Thiago <thiago.angelo@live.com>
2019-05-15 17:58:23 -0300

    implementação da navbar

--- assets/classes/Botao.js
+++ assets/classes/Botao.js
@@ -1,42 +1,76 @@
-const Estilo = require("./Estilo")
+const Estilo = require('./Estilo')
 
 class Botao extends Estilo {
-  constructor(options) {
-    super()
-    this.criar(options)
-    // exemplo de options
-    // {
-    //     icon: 'hamburger',
-    //     width: '60%',
-    //     height: '60%',
-    //     ref: buttons,
-    //     action: () => { console.log('action') }
-    //     style: { backgroundColor: 'black'}
-    // }
-  }
 
-  criar(options) {
-    this.ref = document.createElement("button")
-    const image = document.createElement("img")
-    image.setAttribute("src", `./assets/icons/${options.icon}.svg`)
-    this.addEstilo(image, {
-      width: options.iconWidth,
-      height: options.iconHeight
-    })
-    this.addEstilo(this.ref, {
-      width: options.width,
-      height: options.height,
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: "8px"
-    })
-    this.addEstilo(this.ref, options.style) 
-    this.hover(this.ref, options.hover)
-    this.ref.appendChild(image)
-    this.ref.onclick = options.action
-    options.ref.appendChild(this.ref)
-  }
+
+    constructor(options) {
+        super()
+        this.criar(options)
+        // exemplo de options 
+        // {
+        //     icon: 'hamburger',
+        //     width: '60%',
+        //     height: '60%',
+        //     ref: buttons,
+        //     text: 'texto'
+        //     action: () => { console.log('action') }
+        //     style: { backgroundColor: 'black'}
+        // }
+    }
+
+    criar(options) {
+
+
+        this.ref = document.createElement('button')
+
+        this.addEstilo(this.ref, {
+            width: options.width,
+            height: options.height,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '8px',
+        })
+        this.addEstilo(this.ref, options.style)
+        this.ref.onclick = options.action
+
+        if (options.text) {
+            this.text = document.createElement('p')
+            this.text.textContent = options.text
+            this.addEstilo(this.text, {
+                fontFamily: 'Roboto',
+                color: 'white',
+                fontSize: '1em',
+            })
+            this.ref.appendChild(this.text)
+        }
+        if (options.icon) {
+
+            const image = document.createElement('img')
+            image.setAttribute('src', `./assets/icons/${options.icon}.svg`)
+            this.addEstilo(image, {
+                width: options.imageWidth || '30px',
+                height: options.imageHeight || '30px',
+            })
+
+            this.ref.appendChild(image)
+        }
+        if (options.icon && options.text) {
+
+            this.addEstilo(this.ref, {
+                justifyContent: 'space-around',
+                alignItems: 'center',
+            })
+        }
+
+
+        options.ref.appendChild(this.ref)
+
+
+
+    }
+
+
 }
 
 module.exports = Botao
--- assets/classes/Estilo.js
+++ assets/classes/Estilo.js
@@ -1,29 +1,31 @@
 class Estilo {
-  constructor() {
-    this.canhover = true
-    this.active = false
-  }
-  addEstilo(elemento, estilo) {
-    Object.assign(elemento.style, estilo)
-  }
 
-  hover(elemento, estilo) {
-    const estiloAnterior = {}
+    constructor() {
+        this.canhover = true
+        this.active = false
+    }
+    addEstilo(elemento, estilo) {
+        Object.assign(elemento.style, estilo)
+    }
+    hover(elemento, estilo) {
 
-    Object.assign(estiloAnterior, elemento.style)
+        const estiloAnterior = {}
 
-    elemento.onmouseover = () => {
-      if (this.canhover) {
-        this.addEstilo(elemento, estilo)
-      }
+        Object.assign(estiloAnterior, elemento.style)
+
+        elemento.onmouseover = () => {
+            if (this.canhover) {
+                this.addEstilo(elemento, estilo)
+            }
+        }
+
+        elemento.onmouseout = () => {
+            if (this.canhover) {
+                this.addEstilo(elemento, estiloAnterior)
+            }
+        }
     }
 
-    elemento.onmouseout = () => {
-      if (this.canhover) {
-        this.addEstilo(elemento, estiloAnterior)
-      }
-    }
-  }
 }
 
 module.exports = Estilo
